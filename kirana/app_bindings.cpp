b43d65892b523887b9874ac8b547b191a39f3211
SurajShettigar <suri.shettigar@gmail.com>
2022-11-16 17:05:33 +0100

    Remove window python bindings.
    - Removed window python bindings. Window related functionality is now handled inside core.

--- kirana/app_bindings.cpp
+++ kirana/app_bindings.cpp
@@ -13,26 +13,9 @@ namespace py = pybind11;
 #ifdef COMPILE_BINDINGS
 PYBIND11_MODULE(PY_MODULE_NAME, m)
 {
-    py::class_<kirana::window::Window, std::shared_ptr<kirana::window::Window>>(
-        m, "Window")
-        .def_static("onWindowResize",
-                    &kirana::window::Window::onWindowResizeBindFunc)
-        .def_static("onWindowClose", &kirana::window::Window::onWindowCloseBindFunc)
-        .def("__repr__", [](const kirana::window::Window &w) {
-            return "<kirana.window.Window>";
-        });
-
-    py::class_<kirana::window::PlatformWindow, kirana::window::Window,
-               std::shared_ptr<kirana::window::PlatformWindow>>(
-        m, "PlatformWindow")
-        .def("__repr__", [](const kirana::window::PlatformWindow &w) {
-            return "<kirana.window.PlatformWindow>";
-        });
-
     py::class_<kirana::Application>(m, "Application")
         .def(py::init())
         .def("run", static_cast<void (kirana::Application::*)(
-                        long, int, int)>(&kirana::Application::run))
-        .def("getViewportWindow", &kirana::Application::getViewportWindow);
+                        long, int, int)>(&kirana::Application::run));
 }
 #endif
