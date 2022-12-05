07fa8105bc9fd2c42e9789dc6534efe3de548ace
SurajShettigar <suri.shettigar@gmail.com>
2022-12-05 11:47:44 +0100

    Fix vector using-declaration.

--- kirana/math/vector3.cpp
+++ kirana/math/vector3.cpp
@@ -6,7 +6,9 @@
 #include <iostream>
 #include <string>
 
+using kirana::math::Vector2;
 using kirana::math::Vector3;
+using kirana::math::Vector4;
 
 const Vector3 Vector3::ZERO{0.0f, 0.0f, 0.0f};
 const Vector3 Vector3::ONE{1.0f, 1.0f, 1.0f};
