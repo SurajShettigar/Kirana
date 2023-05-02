f5787307b8de910e0d5d342e5bf1d6cd59502e36
SurajShettigar <suri.shettigar@gmail.com>
2023-05-02 16:23:39 +0200

    [WIP] Parse render pass information to build framegraph.

--- kirana/viewport/vulkan/frame_graph.cpp
+++ kirana/viewport/vulkan/frame_graph.cpp
@@ -2,3 +2,128 @@
 
 #include "vulkan_utils.hpp"
 
+const kirana::viewport::vulkan::FrameGraph::ResourceHandle
+    kirana::viewport::vulkan::FrameGraph::DEPENDENCY_RESOURCE_HANDLE{
+        std::numeric_limits<uint32_t>::max() - 1};
+
+const std::string
+    kirana::viewport::vulkan::FrameGraph::DEPENDENCY_RESOURCE_NAME =
+        "DEPENDENCY_RESOURCE";
+
+kirana::viewport::vulkan::FrameGraph::ResourceHandle kirana::viewport::vulkan::
+    FrameGraph::addOutputResource(const PassOutputResourceData &resourceData)
+{
+    if (m_resourceIndexTable.find(resourceData.name) !=
+        m_resourceIndexTable.end())
+        return ResourceHandle{};
+
+    const ResourceHandle rHandle =
+        static_cast<ResourceHandle>(m_resources.size());
+
+    m_resources.emplace_back(Resource{rHandle, resourceData.name,
+                                      resourceData.type, resourceData.info});
+    m_resourceIndexTable[resourceData.name] = static_cast<uint32_t>(rHandle);
+    return rHandle;
+}
+
+kirana::viewport::vulkan::FrameGraph::NodeHandle kirana::viewport::vulkan::
+    FrameGraph::addNode(const PassData &passData)
+{
+    if (m_nodeIndexTable.find(passData.name) != m_nodeIndexTable.end())
+    {
+        Logger::get().log(constants::LOG_CHANNEL_VULKAN, LogSeverity::error,
+                          "Pass: " + passData.name + " already exists");
+        return NodeHandle{};
+    }
+
+    const NodeHandle nHandle = static_cast<NodeHandle>(m_nodes.size());
+
+    Node node{nHandle, passData.name};
+    // Set node-outputs
+    for (const auto &o : passData.outputs)
+    {
+        const ResourceHandle rHandle = addOutputResource(o);
+        if (!rHandle.isValid())
+        {
+            Logger::get().log(constants::LOG_CHANNEL_VULKAN, LogSeverity::error,
+                              "Output: " + o.name +
+                                  " for pass: " + passData.name +
+                                  " is already being produced by some other "
+                                  "pass");
+            return NodeHandle{};
+        }
+        node.outputs.push_back(rHandle);
+    }
+
+    m_nodes.emplace_back(std::move(node));
+    m_nodeIndexTable[passData.name] = static_cast<uint32_t>(nHandle);
+    return nHandle;
+}
+
+bool kirana::viewport::vulkan::FrameGraph::addPasses(
+    const std::vector<PassData> &passes)
+{
+    for (const auto &p : passes)
+    {
+        const NodeHandle nHandle = addNode(p);
+        if (!nHandle.isValid())
+        {
+            Logger::get().log(constants::LOG_CHANNEL_VULKAN, LogSeverity::error,
+                              "Failed to add pass: " + p.name);
+            return false;
+        }
+    }
+    // Resolve node dependencies and resource inputs
+    for (uint32_t passIdx = 0; passIdx < passes.size(); passIdx++)
+    {
+        Node &node = m_nodes[passIdx];
+        // Add dependency inputs
+        for (const auto &dPass : passes[passIdx].dependentPasses)
+        {
+            if (m_nodeIndexTable.find(dPass) == m_nodeIndexTable.end())
+            {
+                Logger::get().log(constants::LOG_CHANNEL_VULKAN,
+                                  LogSeverity::error,
+                                  "Failed to find dependency pass: " + dPass +
+                                      " for pass: " + node.name);
+                return false;
+            }
+            node.inputs.emplace_back(
+                NodeInput{DEPENDENCY_RESOURCE_HANDLE,
+                          m_nodes.at(m_nodeIndexTable.at(dPass)).id});
+        }
+        // Add resource inputs
+        for (const auto &i : passes[passIdx].inputs)
+        {
+            if (m_resourceIndexTable.find(i.name) == m_resourceIndexTable.end())
+            {
+                Logger::get().log(constants::LOG_CHANNEL_VULKAN,
+                                  LogSeverity::error,
+                                  "Failed to find input resource: " + i.name +
+                                      " for pass: " + node.name);
+                return false;
+            }
+            if (m_nodeIndexTable.find(i.producer) == m_nodeIndexTable.end())
+            {
+                Logger::get().log(
+                    constants::LOG_CHANNEL_VULKAN, LogSeverity::error,
+                    "Failed to find input resource producer: " + i.producer +
+                        " for pass: " + node.name);
+                return false;
+            }
+            node.inputs.emplace_back(
+                NodeInput{m_resources.at(m_resourceIndexTable.at(i.name)).id,
+                          m_nodes.at(m_nodeIndexTable.at(i.producer)).id});
+        }
+    }
+    return true;
+}
+
+bool kirana::viewport::vulkan::FrameGraph::compile()
+{
+
+}
+
+void kirana::viewport::vulkan::FrameGraph::render()
+{
+}
--- kirana/viewport/vulkan/frame_graph.hpp
+++ kirana/viewport/vulkan/frame_graph.hpp
@@ -2,6 +2,7 @@
 #define KIRANA_VIEWPORT_VULKAN_FRAME_GRAPH_HPP
 
 #include "vulkan_types.hpp"
+#include <functional>
 
 namespace kirana::viewport::vulkan
 {
@@ -11,23 +12,195 @@ class Texture;
 class FrameGraph
 {
   public:
+    struct Handle
+    {
+        uint32_t value = std::numeric_limits<uint32_t>::max();
+        Handle() = default;
+        explicit Handle(uint32_t handle) : value{handle} {};
+        explicit operator uint32_t() const
+        {
+            return value;
+        }
+        uint32_t operator()() const
+        {
+            return value;
+        }
+        [[nodiscard]] inline bool isValid() const
+        {
+            return value < std::numeric_limits<uint32_t>::max();
+        }
+    };
+    struct NodeHandle : Handle
+    {
+        NodeHandle() = default;
+        explicit NodeHandle(uint32_t handle) : Handle{handle} {};
+    };
+    struct ResourceHandle : Handle
+    {
+        ResourceHandle() = default;
+        explicit ResourceHandle(uint32_t handle) : Handle{handle} {};
+    };
+    struct FramebufferHandle : Handle
+    {
+        FramebufferHandle() = default;
+        explicit FramebufferHandle(uint32_t handle) : Handle{handle} {};
+    };
+    struct RenderPassHandle : Handle
+    {
+        RenderPassHandle() = default;
+        explicit RenderPassHandle(uint32_t handle) : Handle{handle} {};
+    };
+    enum class ResourceType
+    {
+        UNKNOWN = -1,
+        BUFFER = 0,
+        TEXTURE = 1,
+        ATTACHMENT = 2,
+    };
+    struct ResourceInfo
+    {
+        union {
+            struct
+            {
+                const AllocatedBuffer *buffer;
+            } buffer;
+            struct
+            {
+                std::array<uint32_t, 3> size;
+                vk::AttachmentLoadOp loadOp;
+                const Texture *texture;
+            } texture;
+        };
+    };
+
+    struct PassInputResourceData
+    {
+        /// \brief Name of the input resource.
+        std::string name;
+        /// \brief Name of the pass which produces the input resource.
+        std::string producer;
+    };
+
+    struct PassOutputResourceData
+    {
+        std::string name;
+        ResourceType type;
+        ResourceInfo info;
+    };
+    struct PassData
+    {
+        std::string name;
+        std::vector<PassInputResourceData> inputs;
+        std::vector<PassOutputResourceData> outputs;
+        std::vector<std::string> dependentPasses;
+    };
+
     FrameGraph(const Device *device) : m_device{device} {};
     ~FrameGraph() = default;
 
     FrameGraph(const FrameGraph &frameGraph) = delete;
     FrameGraph &operator=(const FrameGraph &frameGraph) = delete;
 
-    bool addPass(const std::string &passName);
+    bool addPasses(const std::vector<PassData> &passes);
     bool compile();
     void render();
 
   private:
+    // Frame-graph structures
+    struct Resource
+    {
+        ResourceHandle id;
+        std::string name;
+        ResourceType type;
+        ResourceInfo info;
+    };
+    struct NodeInput
+    {
+        /// \brief The resource handle to be used as input to the node.
+        ResourceHandle resource;
+        /// \brief The node which is producing the resource.
+        NodeHandle producer;
+    };
+    struct Node
+    {
+        NodeHandle id;
+        std::string name;
+
+        std::vector<NodeInput> inputs;
+        std::vector<ResourceHandle> outputs;
+
+        FramebufferHandle framebuffer;
+        RenderPassHandle renderPass;
+    };
+
+    static const ResourceHandle DEPENDENCY_RESOURCE_HANDLE;
+    static const std::string DEPENDENCY_RESOURCE_NAME;
+
     bool m_isInitialized = false;
     const Device *const m_device;
+
+    std::unordered_map<std::string, uint32_t> m_resourceIndexTable;
+    std::unordered_map<std::string, uint32_t> m_nodeIndexTable;
+    std::vector<Resource> m_resources;
+    std::vector<Node> m_nodes;
 
     std::vector<const RenderPass *> m_renderPasses;
     std::vector<const Texture *> m_colorAttachments;
     const Texture *m_depthAttachment;
+
+    ResourceHandle addOutputResource(
+        const PassOutputResourceData &resourceData);
+    NodeHandle addNode(const PassData &passData);
 };
 } // namespace kirana::viewport::vulkan
+
+
+// hashing function for handles
+template <> struct std::hash<kirana::viewport::vulkan::FrameGraph::Handle>
+{
+    std::size_t operator()(kirana::viewport::vulkan::FrameGraph::Handle const
+                               &handle) const noexcept
+    {
+        return std::hash<std::uint32_t>{}(handle.value);
+    }
+};
+template <> struct std::hash<kirana::viewport::vulkan::FrameGraph::NodeHandle>
+{
+    std::size_t operator()(
+        kirana::viewport::vulkan::FrameGraph::NodeHandle const &handle)
+        const noexcept
+    {
+        return std::hash<std::uint32_t>{}(handle.value);
+    }
+};
+template <>
+struct std::hash<kirana::viewport::vulkan::FrameGraph::ResourceHandle>
+{
+    std::size_t operator()(
+        kirana::viewport::vulkan::FrameGraph::ResourceHandle const &handle)
+        const noexcept
+    {
+        return std::hash<std::uint32_t>{}(handle.value);
+    }
+};
+template <>
+struct std::hash<kirana::viewport::vulkan::FrameGraph::FramebufferHandle>
+{
+    std::size_t operator()(
+        kirana::viewport::vulkan::FrameGraph::FramebufferHandle const &handle)
+        const noexcept
+    {
+        return std::hash<std::uint32_t>{}(handle.value);
+    }
+};
+template <>
+struct std::hash<kirana::viewport::vulkan::FrameGraph::RenderPassHandle>
+{
+    std::size_t operator()(
+        kirana::viewport::vulkan::FrameGraph::RenderPassHandle const &handle)
+        const noexcept
+    {
+        return std::hash<std::uint32_t>{}(handle.value);
+    }
+};
 #endif
